2d4f984a3a8afca5f03a3d6828bbf195a04cb139
adecuellarv <76180286+adecuellarv@users.noreply.github.com>
2023-05-11 15:54:52 +0200

    validaciones y mensajes en cardpay

--- src/app/components/cardform/cardform.component.ts
+++ src/app/components/cardform/cardform.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core'
 import { Router, ActivatedRoute, Params } from '@angular/router'
+import Swal from 'sweetalert2'
 import { RestService } from 'src/app/services/rest.service'
 import { apiopenpay, apigproducts } from 'src/app/services/config'
 declare var OpenPay: any
@@ -11,7 +12,7 @@ const deviceSessionId = OpenPay.deviceData.setup(
   'payment-form',
   'deviceIdHiddenFieldName'
 )
-
+const timerLoading = 600
 @Component({
   selector: 'app-cardform',
   templateUrl: './cardform.component.html',
@@ -28,47 +29,37 @@ export class CardComponent implements OnInit {
   cvv2: string = ''
   objCard: any
   objPayment: any
-  hideModal: string = 'true'
+  chickedSend: boolean = false
 
   constructor (
     private route: ActivatedRoute,
-    private RestService: RestService,
+    private RestService: RestService
   ) {}
 
-  ngOnInit (): void {
-    
-  }
+  ngOnInit (): void {}
 
   onTypeNumber (event: any) {
     this.card_number = event.target.value
+    this.chickedSend = false
   }
   onTypeName (event: any) {
     this.holder_name = event.target.value
+    this.chickedSend = false
   }
   onTypeExpirationYear (event: any) {
     this.expiration_year = event.target.value
+    this.chickedSend = false
   }
   onTypeExpirationMonth (event: any) {
     this.expiration_month = event.target.value
+    this.chickedSend = false
   }
   onTypeCCV (event: any) {
     this.cvv2 = event.target.value
+    this.chickedSend = false
   }
 
-  SuccessCallbackPayment (response: any) {
-    alert('Success payment')
-    console.log('response_success_payment', response)
-  }
-
-  ErrorCallbackPayment (response: any) {
-    alert('Fallo payment')
-    console.log('response_error_payment', response)
-  }
-
-  SuccessCallbackRegister (response: any) { 
-    //alert('Enviado exitosamente')
-    console.log('response_success', response);
-    
+  SuccessCallbackRegister (response: any) {
     if (response?.data && response?.data?.id) {
       const objPayment = {
         source_id: response?.data?.id,
@@ -86,8 +77,38 @@ export class CardComponent implements OnInit {
       }
 
       try {
-        this.RestService.generalPost(`${apiopenpay}/charge/card`, objPayment).subscribe(resp => {
-          if(resp){
+        this.RestService.generalPost(
+          `${apiopenpay}/charge/card`,
+          objPayment
+        ).subscribe(resp => {
+          if (resp) {
+            const order_id = resp?.data?.id //resp?.data?.order_id
+            if (resp?.data && order_id) {
+              let titleMsg = '', descriptionMsg = '';
+              switch (resp.data?.status) {
+                case 'completed':
+                  titleMsg = 'Pago exitoso'
+                  descriptionMsg =  `En seguida recibiras un correo con tu pago, tu número de referencia es: ${order_id}`;
+                  break;
+                case 'in_progress':
+                  titleMsg = 'Tu pago esta en proceso'
+                  descriptionMsg =  `Aún no hemos pasado tu pago, favor de revisar tu bandeja de correo para ver cambio de estatus o comunicate con nuestros atención a clientes, tu número de referencia es: ${order_id}`;
+                    break;
+                case 'failed':
+                  titleMsg = 'Fallo en pago'
+                  descriptionMsg =  `Tu pago no ha sido procesado favor de volverlo a intentar, tu número de referencia es: ${order_id}`;
+                      break;
+                default:
+                  break;
+              }
+              Swal.fire({
+                icon: 'success',
+                title: titleMsg,
+                text: descriptionMsg,
+                showCancelButton: false,
+                showConfirmButton: true
+              })
+            }
             const objToSave = {
               id_moodle_alumno: this.generalInfo?.userId,
               id_plan_estudio: 1,
@@ -99,43 +120,65 @@ export class CardComponent implements OnInit {
               //id
               //cardinfo
             }
-            this.RestService.generalPost(`${apigproducts}/pasarela/registrar_pago`, objToSave).subscribe(responseRegister => {
-              console.log('save_product_bought', responseRegister);
-            });
-
-          }else{
-            console.log('error_pago');
+            this.RestService.generalPost(
+              `${apigproducts}/pasarela/registrar_pago`,
+              objToSave
+            ).subscribe(responseRegister => {
+              console.log('save_product_bought', responseRegister)
+            })
+          } else {
+            console.log('error_pago')
           }
         })
       } catch (error) {
-        console.log('error_pago'); 
+        console.log('error_pago')
       }
-      
     }
   }
 
   ErrorCallbackRegister (response: any) {
-    alert('Fallo en la transacción')
-    console.log('response_error', response)
+    console.log('error_code', response?.data?.error_code)
+    let text = 'Tuvimos un problema al procesar tu pago, favor de intentarlo nuevamente';
+    if(response?.data?.error_code === 2004 || response?.data?.error_code === 2005 || response?.data?.error_code === 2006){
+      text = 'Favor de revisar tus datos nuevamente'
+    }
+    setTimeout(() => {
+      Swal.fire({
+        icon: 'error',
+        title: '¡Pago fallido!',
+        text,
+        footer: ''
+      })
+    }, timerLoading)
   }
 
   sendpay () {
-    this.objCard = {
-      card_number: this.card_number,
-      holder_name: this.holder_name,
-      expiration_year: this.expiration_year,
-      expiration_month: this.expiration_month,
-      cvv2: this.cvv2,
-      amount: this.generalInfo?.total.toFixed(2),
-      description: this.generalInfo.nameProduct
-    }
+    this.chickedSend = true
 
-    OpenPay.token.create(
-      this.objCard,
-      (successResponse: any) => {
-        this.SuccessCallbackRegister(successResponse);
-      },
-      this.ErrorCallbackRegister
-    )
+    if (
+      this.card_number &&
+      this.holder_name &&
+      this.expiration_year &&
+      this.expiration_month &&
+      this.cvv2
+    ) {
+      this.objCard = {
+        card_number: this.card_number,
+        holder_name: this.holder_name,
+        expiration_year: this.expiration_year,
+        expiration_month: this.expiration_month,
+        cvv2: this.cvv2,
+        amount: this.generalInfo?.total.toFixed(2),
+        description: this.generalInfo.nameProduct
+      }
+      Swal.showLoading()
+      OpenPay.token.create(
+        this.objCard,
+        (successResponse: any) => {
+          this.SuccessCallbackRegister(successResponse)
+        },
+        this.ErrorCallbackRegister
+      )
+    }
   }
 }
